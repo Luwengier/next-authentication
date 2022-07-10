baf0987b9430afc6dd364cc2952daf705e8ccf1a
Luwengier <g217881111@gmail.com>
2022-07-10 16:40:40 +0800

    Add sign in logic and API route

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,29 @@
+import { hashPassword } from '../../../lib/auth'
+import { connectToDatabase } from '../../../lib/db'
+
+
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return
+  }
+
+  const data = req.body
+  const { email, password } = data
+  console.log('Hi')
+
+  if (!email || !email.includes('@') || !password || password.trim().length < 7) {
+    res.status(422).json({ message: 'Invalid input.' })
+  }
+
+  const client = await connectToDatabase()
+  const db = client.db('auth-demo')
+
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashPassword(password),
+  })
+
+  res.status(201).json({ message: 'Create user!' })
+}
+
+export default handler
