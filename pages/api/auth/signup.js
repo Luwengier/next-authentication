d08259c8c01e0a54e5a78fdb95b0517e348517ff
Luwengier <g217881111@gmail.com>
2022-07-15 23:54:57 +0800

    Improve signup with unique email address

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -17,12 +17,20 @@ async function handler(req, res) {
   const client = await connectToDatabase()
   const db = client.db('auth-demo')
 
+  const existingUser = await db.collection('users').findOne({ email: email })
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already!' })
+    client.close()
+    return
+  }
+
   const result = await db.collection('users').insertOne({
     email: email,
     password: await hashPassword(password),
   })
 
   res.status(201).json({ message: 'Create user!' })
+  client.close()
 }
 
 export default handler
