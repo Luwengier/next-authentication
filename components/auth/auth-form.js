49d048f9aede8744f1dbcbb453541389382be92f
Luwengier <g217881111@gmail.com>
2022-07-26 03:11:23 +0800

    Add client side and server side page guard

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react'
 import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import classes from './auth-form.module.css'
 
 async function createUser(email, password) {
@@ -23,7 +24,9 @@ async function createUser(email, password) {
 function AuthForm() {
   const emailRef = useRef()
   const passwordRef = useRef()
+
   const [isLogin, setIsLogin] = useState(true)
+  const router = useRouter()
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState)
@@ -42,7 +45,10 @@ function AuthForm() {
         password: enteredPassword,
       })
 
-      console.log(result)
+      if (!result.error) {
+        router.replace('/profile')
+      }
+
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword)
